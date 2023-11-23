75ab2eba8aef612a01f2f28d8e335f61ad3d0adf
Abel Brencsan <abel@brencsan.hu>
2023-11-23 18:16:38 +0100

    Update build script

--- build.py
+++ build.py
@@ -1,10 +1,17 @@
+"""Script for deploying and building styles and scripts"""
+
 # Import modules
 import os
+import json
+import argparse
 
-import pyinotify
+from fabric import Connection
+import jsmin
 
 # Init variables
 paths = {
+    "root": os.path.dirname(os.path.abspath(__file__)) + "/",
+    "sftp_config": "sftp-config.json",
     "style": {
         "root": "assets/css",
         "destination": "app.css",
@@ -15,83 +22,91 @@ paths = {
         "destination": "scripts.js",
         "sources": [
             "focus-tracker.js",
-            "app.js"
+            "app.js",
         ],
     },
 }
 
-def buildScripts(paths):
-    """Merge together given scripts
-    
-    paths
-        A dictionary of paths
-    """
-    
+def build_scripts(upload):
+    """Merge together given scripts"""
+
     # Init variables
-    src_filenames = [os.path.join(paths["root"], f) for f in paths["sources"]]
-    dest_filename = os.path.join(paths["root"], paths["destination"])
+    src_filenames = [os.path.join(paths["script"]["root"], f) for f in paths["script"]["sources"]]
+    dest_filename = os.path.join(paths["script"]["root"], paths["script"]["destination"])
     scripts = ""
 
     # Merge scipts
     for src_filename in src_filenames:
-        with open(src_filename, "r") as file:
+        with open(paths['root'] + src_filename, "r", encoding="utf-8") as file:
             scripts += file.read()
-    with open(dest_filename, "w") as file:
-        file.write(scripts)
+    with open(paths['root'] + dest_filename, "w", encoding="utf-8") as file:
+        file.write(jsmin.jsmin(scripts))
 
     # Print results
     print("Scripts are built sucessfully.")
+    if upload and upload_to_server(dest_filename):
+        print("Merged script is uploaded to server")
 
 
-def buildStyles(paths):
-    """Build CSS from given SCSS files
-    
-    paths
-        A dictionary of paths
-    """
+def build_styles(upload):
+    """Build CSS from given SCSS files"""
 
-    src_filename = os.path.abspath(os.path.join(paths["root"], paths["source"]))
-    dest_filename = os.path.abspath(os.path.join(paths["root"], paths["destination"]))
-    os.system("sass {0} {1} --style=compressed --no-source-map".format(src_filename, dest_filename))
+    src_filename = os.path.join(paths["style"]["root"], paths["style"]["source"])
+    dest_filename = os.path.join(paths["style"]["root"], paths["style"]["destination"])
+    os.system(f"sass {paths['root'] + src_filename} {paths['root'] + dest_filename} --style=compressed --no-source-map")
 
     # Print results
     print("Styles are built sucessfully.")
+    if upload and upload_to_server(dest_filename):
+        print("Merged style is uploaded to server")
 
 
-def watchFiles(paths):
-    """Build CSS and JS files on file change
+def upload_to_server(dest_filename):
+    """Upload given file to server trough SSH
     
-    paths
-        A dictionary of style and script paths
+    dest_filename
+        Local filename to be uploaded
     """
 
-    class EventProcessor(pyinotify.ProcessEvent):
-        def process_IN_CLOSE_WRITE(self, event):
-            buildScripts(paths["script"])
-            buildStyles(paths["style"])
+    # Try to load config file
+    config = read_sftp_config()
+    if config is None or "remote_path" not in config or "host" not in config:
+        return False
+
+    # Upload file to server
+    remote_file = os.path.abspath(os.path.join(config["remote_path"], dest_filename))
+    with Connection(host=config["host"], user=config["user"]) as conn:
+        conn.put(paths['root'] + dest_filename, remote=remote_file)
+        return True
+
+    return False
 
 
-    # Init variables
-    wm = pyinotify.WatchManager()
-    en = pyinotify.Notifier(wm, EventProcessor())
-    watched = []
+def read_sftp_config():
+    """Try to read SSH configuration"""
 
-    # Set watched script files
-    for filename in paths["script"]["sources"]:
-        watched.append(os.path.join(paths["script"]["root"], filename))
+    # Read file
+    filename = paths['root'] + paths["sftp_config"]
+    if filename is not None and os.path.isfile(filename):
+        with open(filename, "r", encoding="utf-8") as file:
+            return json.load(file)
 
-    # Set watched style files
-    style_dir = os.listdir(paths["style"]["root"])
-    for filename in style_dir:
-        if filename not in [paths["style"]["source"], paths["style"]["destination"]]:
-            watched.append(os.path.join(paths["style"]["root"], filename))
-
-    # Watch files
-    wm.add_watch(watched, pyinotify.ALL_EVENTS)
-    en.loop()
+    return None
 
 
-# Build scripts and styles on startup
-buildScripts(paths["script"])
-buildStyles(paths["style"])
-watchFiles(paths)
+if __name__ == '__main__':
+
+    # Init parser
+    parser = argparse.ArgumentParser(description=__doc__)
+
+    # Parse arguments
+    parser.add_argument('-c', '--styles', action='store_true', help='build styles')
+    parser.add_argument('-s', '--scripts', action='store_true', help='build scripts')
+    parser.add_argument('-u', '--upload', action='store_true', help='upload build trough SSH')
+    args = parser.parse_args()
+
+    # Build scripts and styles on startup
+    if args.styles:
+        build_styles(args.upload)
+    if args.scripts:
+        build_scripts(args.upload)
